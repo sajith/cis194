8cff98e1f2f087fa0228502a441695ebc145f504
Sajith Sasidharan <sajith@nonzen.in>
2015-07-15 11:05:54 -0400

    Closer..

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -3,9 +3,10 @@ module Golf where
 
 -- TODO: fix this
 skips :: [a] -> [[a]]
-skips xs = [xs] ++ map (\n -> skip n xs) [1..length xs-1]
-
-skip :: Int -> [a] -> [a]
-skip n [] = []
-skip n xs = (take n $ drop n xs) ++ skip n (drop (2*n) xs)
+skips xs = map (`skipn` xs) [1..length xs-1]
+    where
+        skipn :: Int -> [a] -> [a]
+        skipn n xs' = case drop (n-1) xs' of
+            (y:ys) -> y : skipn n ys
+            []     -> []
 

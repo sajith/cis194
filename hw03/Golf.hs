d564145329e8a06cec071d73be228faa2b8eeaee
Sajith Sasidharan <sajith@nonzen.in>
2015-07-15 11:07:36 -0400

    Add hw03/skips -- examples work.

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -1,9 +1,8 @@
 
 module Golf where
 
--- TODO: fix this
 skips :: [a] -> [[a]]
-skips xs = map (`skipn` xs) [1..length xs-1]
+skips xs = map (`skipn` xs) [1..length xs]
     where
         skipn :: Int -> [a] -> [a]
         skipn n xs' = case drop (n-1) xs' of

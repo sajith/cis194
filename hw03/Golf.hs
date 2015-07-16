a7809278a28eb7c2f604865ba867a2d87a64322e
Sajith Sasidharan <sajith@nonzen.in>
2015-07-16 16:21:20 -0400

    Add comments to hw03/histogram

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -30,18 +30,23 @@ histogram xs = hist xs ++ "\n==========\n0123456789\n"
                 $ transpose
                 $ map (\e -> histLine e (maxOccur xs)) $ times xs
 
+        -- filter numbers not in [0..9] out.
         focus :: [Integer] -> [Integer]
         focus = filter (`elem` [0..9])
 
+        -- count occurances of n in xs
         occur :: Integer -> [Integer] -> Int
         occur n xs = length $ filter (== n) xs
 
+        -- make a list of occurances [0..9]
         times :: [Integer] -> [Int]
         times xs = map (\n -> occur n (focus xs)) [0..9]
 
+        -- find the maximum number of occurances.
         maxOccur :: [Integer] -> Int
         maxOccur xs = maximum $ times xs
 
+        -- form a line in the (horizontal) histogram.
         histLine :: Int -> Int -> String
         histLine rep max = replicate (max - rep) ' ' ++
                            replicate rep '*'

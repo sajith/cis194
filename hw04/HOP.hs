fd7cffac28c212e6a6b5255e87f802dcdee0cd45
Sajith Sasidharan <sajith@nonzen.in>
2015-07-17 11:42:31 -0400

    Add hw04/fun1

---
+++ hw04/HOP.hs
@@ -0,0 +1,23 @@
+module HOP where
+
+------------------------------------------------------------
+
+-- fun1 :: [Integer] -> Integer
+-- fun1 []     = 1
+-- fun1 (x:xs) | even x  = (x-2) * fun1 xs
+--             | otherwise = fun1 xs
+
+fun1 :: [Integer] -> Integer
+fun1 = product . map (\n -> if even n then n-2 else 1)
+
+-- fun2 :: Integer -> Integer
+-- fun2 1 = 0
+-- fun2 n | even n    = n + fun2 (n `div` 2)
+--        | otherwise = fun2 (3 * n + 1)
+
+-- TODO: could not figure out
+fun2 :: Integer -> Integer
+fun2 = undefined
+
+------------------------------------------------------------
+

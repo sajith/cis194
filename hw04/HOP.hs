11a79bbc52b8674e2b34d153a64179bcded9079d
Sajith Sasidharan <sajith@nonzen.in>
2015-07-17 23:48:20 -0400

    Completed hw04/foldTree.
    Pretty sure this is a balanced binary tree.

--- hw04/HOP.hs
+++ hw04/HOP.hs
@@ -22,17 +22,30 @@ fun2 = sum . filter even . takeWhile (>1) . iterate fn
 
 ------------------------------------------------------------
 
--- TODO: I'm lost.
-
 data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
             deriving (Eq, Show)
 
 foldTree :: [a] -> Tree a
-foldTree = undefined
+foldTree = foldr insert Leaf
 
-insert :: Tree a -> a -> Tree a
-insert = undefined
+insert :: a -> Tree a -> Tree a
+insert a Leaf                  = Node 0 Leaf a Leaf
+insert a (Node h Leaf a' Leaf) = Node (h+1) (Node 0 Leaf a Leaf) a' Leaf
+insert a (Node h Leaf a' r)    = Node h (Node 0 Leaf a Leaf) a' r
+insert a n@(Node h l a' r)     = if height l == h - 1
+                                 then Node (newHeight n) l a' (insert a r)
+                                 else Node (newHeight n) (insert a l) a' r
+
+height :: Tree a -> Integer
+height Leaf           = 0
+height (Node h _ _ _) = h
+
+newHeight :: Tree a -> Integer
+newHeight (Node _ l _ r) = if (height l > height r)
+                           then height l + 1
+                           else height r + 1
+newHeight Leaf           = error "something terrible happened"
 
 ------------------------------------------------------------
 

7b7b7e392535912f420fc7b31d891854c0df754f
Sajith Sasidharan <sajith@nonzen.in>
2015-07-20 10:54:13 -0400

    Add hw05/evalStr

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -1,8 +1,20 @@
 module Calc where
 
 import           ExprT
+import           Parser
+
+import           Control.Applicative ((<$>))
+
+------------------------------------------------------------
 
 eval :: ExprT -> Integer
 eval (Lit n)     = n
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
+
+------------------------------------------------------------
+
+evalStr :: String -> Maybe Integer
+evalStr str = eval <$> parseExp Lit Add Mul str
+
+------------------------------------------------------------

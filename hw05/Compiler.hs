f7dccb240197bd04195f66756b083c23be70f4f6
Sajith Sasidharan <sajith@nonzen.in>
2015-07-20 23:28:41 -0400

    Add incomplete hw05 excercise 5

--- hw05/Compiler.hs
+++ hw05/Compiler.hs
@@ -7,9 +7,9 @@ import qualified Calc    as C
 import qualified StackVM as S
 
 instance C.Expr S.Program where
-    lit = undefined
-    add = undefined
-    mul = undefined
+    lit x   = [S.PushI x]
+    add x y = x ++ y ++ [S.Add]
+    mul x y = x ++ y ++ [S.Mul]
 
 compile :: String -> Maybe S.Program
 compile = undefined

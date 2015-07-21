7d2444511eb62a3f6cdfe2632e16fcd047a03bb0
Sajith Sasidharan <sajith@nonzen.in>
2015-07-20 23:35:37 -0400

    Kinda sorta implement hw05 excercise 5

--- hw05/Compiler.hs
+++ hw05/Compiler.hs
@@ -4,6 +4,7 @@
 module Compiler where
 
 import qualified Calc    as C
+import qualified Parser  as P
 import qualified StackVM as S
 
 instance C.Expr S.Program where
@@ -12,5 +13,5 @@ instance C.Expr S.Program where
     mul x y = x ++ y ++ [S.Mul]
 
 compile :: String -> Maybe S.Program
-compile = undefined
+compile = P.parseExp C.lit C.add C.mul
 

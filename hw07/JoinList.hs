051da36fe123e08d5009bc0f43ac7fe17b4dcc32
Sajith Sasidharan <sajith@nonzen.in>
2015-07-24 18:14:18 -0400

    Add JoinList

---
+++ hw07/JoinList.hs
@@ -0,0 +1,7 @@
+
+module JoinList where
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)

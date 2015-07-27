921d0580e2888c4f262f2313ce4f3c284d360a53
Sajith Sasidharan <sajith@nonzen.in>
2015-07-27 06:13:25 -0400

    Add hw07/tag

--- hw07/JoinList.hs
+++ hw07/JoinList.hs
@@ -1,7 +1,25 @@
 
 module JoinList where
 
+import           Data.Monoid
+
+------------------------------------------------------------
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
+
+------------------------------------------------------------
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) = undefined
+
+------------------------------------------------------------
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = error "empty"
+tag (Single m _)   = m
+tag (Append m _ _) = m
+
+------------------------------------------------------------

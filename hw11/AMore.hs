d0a744f82646fff7f7ff27824bab1777826eee32
Sajith Sasidharan <sajith@nonzen.in>
2015-07-31 15:00:31 -0400

    Add hw11 empty zeroOrMore and oneOrMore

---
+++ hw11/AMore.hs
@@ -0,0 +1,16 @@
+
+module AMore where
+
+import           Data.Char (isUpper)
+
+import           AParser
+
+------------------------------------------------------------
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = _
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore  = _
+
+------------------------------------------------------------

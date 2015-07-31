0a404d4c22e8c8bed95e54ed72aa33d557cdf94d
Sajith Sasidharan <sajith@nonzen.in>
2015-07-31 10:59:42 -0400

    Remove unused variable.

--- hw10/ATest.hs
+++ hw10/ATest.hs
@@ -17,7 +17,7 @@ instance Functor Parser where
 
 instance Applicative Parser where
     pure a = Parser (\s -> Just (a, s))
-    (<*>) p1@(Parser f) p2@(Parser p) =
+    (<*>) (Parser f) p2@(Parser p) =
         Parser $ \s ->
             case f s of
                 Nothing       -> Nothing

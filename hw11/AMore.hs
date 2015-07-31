f6971876770bc1260ed15d37393dd52b67c6728d
Sajith Sasidharan <sajith@nonzen.in>
2015-07-31 15:02:21 -0400

    Add zeroOrMore

--- hw11/AMore.hs
+++ hw11/AMore.hs
@@ -9,7 +9,7 @@ import AParser
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = _
+zeroOrMore p = pure [] <|> oneOrMore p
 
 oneOrMore :: Parser a -> Parser [a]
 oneOrMore  = _

7a8fdea196359e253ee8c54b9241ef2574d157c7
Sajith Sasidharan <sajith@nonzen.in>
2015-08-05 16:02:08 -0400

    Add dieRoll

--- hw12/Solution.hs
+++ hw12/Solution.hs
@@ -6,7 +6,17 @@ import           Risk
 
 ------------------------------------------------------------
 
+dieRoll :: Battlefield -> (Army, Army)
+dieRoll field | attackers field <= 1 = (0, defenders field)
+              | defenders field <= 0 = (attackers field, 0)
+              | otherwise            = (min (attackers field) 3, min (defenders field) 2)
+
+
 battle :: Battlefield -> Rand StdGen Battlefield
-battle field = undefined
+battle field = do
+    let (attackers, defenders) = dieRoll field
+    deadAttackers <- undefined
+    deadDefenders <- undefined
+    return field
 
 ------------------------------------------------------------

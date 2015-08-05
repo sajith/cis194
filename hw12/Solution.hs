4b18019422f89e8d54d7677943f393389d0864e9
Sajith Sasidharan <sajith@nonzen.in>
2015-08-05 16:59:50 -0400

    successProb WIP

--- hw12/Solution.hs
+++ hw12/Solution.hs
@@ -38,10 +38,8 @@ invade field@(Battlefield att def) = if def > 0 || att > 2
 
 successProb :: Battlefield -> Rand StdGen Double
 successProb field = do
-    all <- mapM invade (replicate n field)
+    all <- mapM invade (replicate 1000 field)
     let defLoss = 0.0 :: Double
-    return $ defLoss / (toEnum n)
-    where
-        n = 1000
+    return $ defLoss / 1000
 
 ------------------------------------------------------------

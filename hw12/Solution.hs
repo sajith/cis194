b62db01b748a21448298fb36845104810ee3be04
Sajith Sasidharan <sajith@nonzen.in>
2015-08-05 13:13:53 -0400

    Begin hw12 battle

---
+++ hw12/Solution.hs
@@ -0,0 +1,12 @@
+
+module Solution where
+
+import           Control.Monad.Random
+import           Risk
+
+------------------------------------------------------------
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle field = undefined
+
+------------------------------------------------------------
